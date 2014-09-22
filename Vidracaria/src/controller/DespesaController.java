5d3dc8ae03b9afad1f0eec2faf9deeb9977ce855
rafael <rafael@Debian>
2014-09-22 17:36:20 -0300

    Complementando o commit anterior (tinha faltado o controller)

--- Vidracaria/src/controller/DespesaController.java
+++ Vidracaria/src/controller/DespesaController.java
@@ -1,10 +1,28 @@
 
 package controller;
 
+import domain.Despesa;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author rafael
  */
 public class DespesaController {
     
+    public boolean validarDespesa(Despesa despesa) {
+        if(String.valueOf(despesa.getCodEmpresa()).isEmpty()) {
+            JOptionPane.showMessageDialog(null, "O preenchimento do codigo da empresa é obrigatório.");
+            return false;
+        }
+        else if(String.valueOf(despesa.getCodTipoDespesa()).isEmpty()) {
+            JOptionPane.showMessageDialog(null, "O preenchimento do codigo do tipo de despesa é obrigatório.");
+            return false;                   
+        }
+        else if(despesa.getMesAno().isEmpty()) {
+            JOptionPane.showMessageDialog(null, "O preenchimento do mes/ano da despesa é obrigatório.");
+            return false;
+        }
+        return true;
+    }    
 }
