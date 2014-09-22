19a860f417386edffa10d0b92171e851363cec7c
rafael <rafael@Debian>
2014-09-22 19:56:16 -0300

    Controller da classe Empresa (tinha faltado no ultimo commit)

--- Vidracaria/src/controller/EmpresaController.java
+++ Vidracaria/src/controller/EmpresaController.java
@@ -1,10 +1,28 @@
-
 package controller;
+
+import domain.Empresa;
+import javax.swing.JOptionPane;
 
 /**
  *
  * @author rafael
  */
 public class EmpresaController {
-    
+
+    public boolean validarEmpresa(Empresa empresa) {
+        if (String.valueOf(empresa.getCodEmpresa()).isEmpty()) {
+            JOptionPane.showMessageDialog(null, "O preenchimento do codigo da mpresa é obrigatório");
+            return false;
+        } else if (empresa.getCnpjEmpresa().isEmpty()) {
+            JOptionPane.showMessageDialog(null, "O preenchimento do cnpj da empresa é obrigatório");
+            return false;
+        } else if (empresa.getRazaoSocial().isEmpty()) {
+            JOptionPane.showMessageDialog(null, "O preenchimento da razao social da empresa é obrigatório");
+            return false;
+        } else if (empresa.getNomeFantasia().isEmpty()) {
+            JOptionPane.showMessageDialog(null, "O preenchimento do nome fantasia da empresa é obrigtório");
+            return false;
+        }
+        return true;
+    }
 }
