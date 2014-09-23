80d11dad63be6ade8c371e4eeaa0b6a93155adaa
rafael <rafael@Debian>
2014-09-23 00:29:00 -0300

    continuando.. classe parcelamento p2

--- Vidracaria/src/controller/ParcelamentoController.java
+++ Vidracaria/src/controller/ParcelamentoController.java
@@ -1,10 +1,26 @@
-
 package controller;
+
+import domain.Parcelamento;
+import javax.swing.JOptionPane;
 
 /**
  *
  * @author rafael
  */
 public class ParcelamentoController {
-    
+
+    public boolean validarParcelamento(Parcelamento parcelamento) {
+        if (String.valueOf(parcelamento.getCodParcelamento()).isEmpty()) {
+            JOptionPane.showMessageDialog(null, "O preenchimento do codigo do parcelamento é obrigatório.");
+            return false;
+        } else if (parcelamento.getDescricaoParcelamento().isEmpty()) {
+            JOptionPane.showMessageDialog(null, "O preenchimento da descrição do parcelamento é obrigatório.");
+            return false;
+        } else if (String.valueOf(parcelamento.getQuantidadeParcelas()).isEmpty()) {
+            JOptionPane.showMessageDialog(null, "O preenchimento da quantidade de parcelas é obrigatório.");
+            return false;
+        }
+        return true;
+    }
+
 }
